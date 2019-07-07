ef531246053011dec117b18c4c536a8f0d490cf6
Will Cassella <wcassella@gmail.com>
2019-07-07 12:49:08 -0700

    Refactored some components of the API

--- src/ui.rs
+++ src/ui.rs
@@ -13,16 +13,16 @@ impl Element for BlueBox {
     fn run(
         &self,
         mut ctx: Context,
+        id: Id,
     ) -> LayoutObj {
-        let id = ctx.element_id();
         let max_area = ctx.max_area();
-        let mut b = ctx.begin_element(max_area, id.append_str("border"), Border::uniform(10_f32).color(color::RGBA8(0x10_C0_C9_FF)));
+        let mut b = ctx.begin_element(max_area, id.str("border"), Border::uniform(10_f32).color(color::RGBA8(0x10_C0_C9_FF)));
 
         Fill::new(color::constants::WHITE)
-        .begin(&mut b, SocketName::default(), id.append_str("fill"));
+        .begin(&mut b, SocketName::default(), id.str("fill"));
 
             Size::default().width(20_f32).height(10_f32)
-            .begin(&mut b, SocketName::default(), id.append_str("inner")).end();
+            .begin(&mut b, SocketName::default(), id.str("inner")).end();
 
         b.end(); // fill
 
@@ -36,14 +36,15 @@ impl Element for Repeating {
     fn run(
         &self,
         mut ctx: Context,
+        id: Id,
     ) -> LayoutObj {
         let max_area = ctx.max_area();
-        let mut b = ctx.begin_element(max_area, Id::from("Repeating_stack"), List::left_to_right());
+        let mut b = ctx.begin_element(max_area, id.str("list"), List::left_to_right());
 
-        for _ in 0..100 {
-            Border::default().top(15_f32).bottom(15_f32).right(30_f32).begin(&mut b, SocketName::default(), Id::from("padding"));
-                Size::default().height(100_f32).v_align(VAlign::Center).begin(&mut b, SocketName::default(), Id::from("size"));
-                    BlueBox.begin(&mut b, SocketName::default(), Id::from("BlueBox")).end(); // BlueBox
+        for i in 0..100 {
+            Border::default().top(15_f32).bottom(15_f32).right(30_f32).begin(&mut b, SocketName::default(), id.str("padding").num(i));
+                Size::default().height(100_f32).v_align(VAlign::Center).begin(&mut b, SocketName::default(), id.str("size").num(i));
+                    BlueBox.begin(&mut b, SocketName::default(), id.str("BlueBox").num(i)).end(); // BlueBox
                 b.end(); // size
             b.end(); // padding
         }
